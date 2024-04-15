bcdc0b48f67e17827cdf45b14cfa220241b60d79
jeonDev <jeonDev>
2024-04-15 16:17:25 +0900

    actuator

---
+++ src/test/java/com/monitor/core/MonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitor.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
